551b68bcea11e6e31e75c1533afd7dc6af600712
IbyGit <italobbn@hotmail.com>
2023-06-01 01:41:40 -0300

    Exits on Editor Now

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
     }
 
     public void ExitGame () {
+    #if UNITY_STANDALONE
         Application.Quit();
+    #endif
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
     }
 }
