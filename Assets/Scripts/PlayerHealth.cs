a8201558d62cd29a0f3e9b6842f7f9b36fe3ba2f
IbyGit <italobbn@hotmail.com>
2023-06-01 04:03:34 -0300

    idk

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static event Action OnPlayerDamaged;
+    public static event Action OnPlayerDeath;
+
+    public int maxHealth = 3;
+    public int health;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage (int amount) {
+        health -= amount;
+        if (health <= 0)
+        {
+            anim.SetBool("IsDead", true);
+            //todo show game over screen
+        }
+    }
+}
