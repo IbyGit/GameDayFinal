1a6f98dae9b0fb5b141b0d127353efea1b1949de
IbyGit <italobbn@hotmail.com>
2023-06-01 12:36:28 -0300

    Box ground check done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D coll;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
@@ -43,7 +44,8 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded ()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        // return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
     }
 
     private void Flip()
